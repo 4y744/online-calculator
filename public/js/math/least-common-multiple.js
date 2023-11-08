4992c769be7f5f7481d9f23f1fc33f9f25ee0a0c
Aytach Salif <ayta42007@gmail.com>
2023-11-09 00:48:33 +0200

    added lcm

--- public/js/math/least-common-multiple.js
+++ public/js/math/least-common-multiple.js
@@ -1,6 +1,6 @@
 let input_box = document.getElementById("input-box");
 let output_label = document.getElementById("output-label");
-let input, num1, num2;
+let input, num1, num2, lcm;
 
 function calculate()
 {
@@ -9,13 +9,22 @@ function calculate()
 
     for(let i = 0; i < input.length; i++)
     {
-        if(i == 0) {num1 = parseFloat(input[i]); continue;}
+        if(i == 0) 
+        {
+            lcm = parseFloat(input[i]);
+            continue;
+        }
+        num1 = lcm;
         num2 = parseFloat(input[i]);
         while(num1 != num2)
         {
             if(num1 > num2) {num1 -= num2;}
             else {num2 -= num1;}
-        }
+        } 
+
+        lcm = (parseFloat(input[i]) * lcm) / num1;
     }
-   output_label.innerText = `Answer: ${num1}`;
+
+
+   output_label.innerText = `Answer: ${lcm}`;
 }
